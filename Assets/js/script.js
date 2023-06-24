1ea1629f367808dfbe6705661fecd3bdedd6a8d8
cumauu73 <cumauu73@icloud.com>
2023-06-24 09:25:08 -0500

    first commit

---
+++ Assets/js/script.js
@@ -0,0 +1,16 @@
+//timer code
+//when i click the start quiz time start 50 seconds--
+//display at the class timer time:
+//when ist zero show timeis up
+//show high score
+//show start again
+
+
+
+
+
+
+
+
+
+//quiz code
