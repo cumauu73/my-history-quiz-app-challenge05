37b90d5aee60c3e87e10bf6acbfc34ef1f4d8676
cumauu73 <cumauu73@icloud.com>
2023-06-25 23:12:37 -0500

    first try

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,134 +21,139 @@
 //quiz code
 // Quiz questions and answers
 
-// var questions = [
-//   {
-//     question: "Question 1",
-//     choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-//     correctAnswer: 2
-//   },
-//   {
-//     question: "Question 2",
-//     choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-//     correctAnswer: 1
-//   },
-//   // Add more questions here
-// ];
+var questions = [
+  {
+    question: "when its ww3?",
+    choices: ["now", "2024", "never"],
+    correctAnswer: 2
+  },
+  {
+    question: "how old are you?",
+    choices: ["18", "45", "90"],
+    correctAnswer: 1
+  },
+  {
+    question: "what is your favorite color?",
+    choices: ["red","blue","purple"],
+    correctAnswer: 2
+  }
+  
+]
 
-// // Global variables
-// var startButton = document.getElementById("start");
-// var quizContainer = document.getElementById("quiz");
-// var questionElement = document.getElementById("question");
-// var choicesElement = document.getElementById("choices");
-// var gameOverElement = document.getElementById("game-over");
-// var scoreElement = document.getElementById("final-score");
-// var initialsInput = document.getElementById("initials");
-// var saveScoreButton = document.getElementById("save-score");
-// var playAgainButton = document.getElementById("play-again-button");
-// var timeLeftElement = document.getElementById("time-left");
+// Global variables
+var startButton = document.getElementById("start");
+var quizContainer = document.getElementById("quiz");
+var questionElement = document.getElementById("question");
+var choicesElement = document.getElementById("choices");
+var gameOverElement = document.getElementById("game-over");
+var scoreElement = document.getElementById("final-score");
+var initialsInput = document.getElementById("initials");
+var saveScoreButton = document.getElementById("save-score");
+var playAgainButton = document.getElementById("play-again-button");
+var timeLeftElement = document.getElementById("time-left");
 
-// var currentQuestionIndex = 0;
-// var timeLeft = 60;
-// var timerInterval;
-// var score = 0;
+var currentQuestionIndex = 0;
+var timeLeft = 60;
+var timerInterval;
+var score = 0;
 
-// // Event listener for start button
-// startButton.addEventListener("click", startQuiz);
+// Event listener for start button
+startButton.addEventListener("click", startQuiz);
 
-// // Event listener for choice buttons
-// choicesElement.addEventListener("click", selectAnswer);
+// Event listener for choice buttons
+choicesElement.addEventListener("click", selectAnswer);
 
-// // Event listener for save score button
-// saveScoreButton.addEventListener("click", saveScore);
+// Event listener for save score button
+saveScoreButton.addEventListener("click", saveScore);
 
-// // Event listener for play again button
-// playAgainButton.addEventListener("click", resetQuiz);
+// Event listener for play again button
+playAgainButton.addEventListener("click", resetQuiz);
 
-// // Starts the quiz
-// function startQuiz() {
-//   startButton.style.display = "none";
-//   quizContainer.style.display = "block";
-//   timerInterval = setInterval(updateTime, 1000);
-//   updateTime();
-//   showQuestion();
-// }
+// Starts the quiz
+function startQuiz() {
+  startButton.style.display = "none";
+  quizContainer.style.display = "block";
+  timerInterval = setInterval(updateTime, 1000);
+  updateTime();
+  showQuestion();
+}
 
-// // Displays a question and its choices
-// function showQuestion() {
-//   var question = questions[currentQuestionIndex];
-//   questionElement.textContent = question.question;
+// Displays a question and  choices
+function showQuestion() {
+  var question = questions[currentQuestionIndex];
+  questionElement.textContent = question.question;
 
-//   choicesElement.innerHTML = "";
-//   question.choices.forEach(function (choice, index) {
-//     var choiceButton = document.createElement("button");
-//     choiceButton.setAttribute("class", "choice");
-//     choiceButton.textContent = choice;
-//     choicesElement.appendChild(document.createElement("li").appendChild(choiceButton));
-//   });
-// }
+  choicesElement.innerHTML = "";
+  question.choices.forEach(function (choice, index) {
+    var choiceButton = document.createElement("button");
+    choiceButton.setAttribute("class", "choice");
+    choiceButton.textContent = choice;
+    choicesElement.appendChild(document.createElement("li").appendChild(choiceButton));
+  });
+}
 
-// // Handles the selection of an answer
-// function selectAnswer(event) {
-//   var selectedButton = event.target;
-//   if (!selectedButton.classList.contains("choice")) {
-//     return;
-//   }
+// Handles the selection of an answer
+function selectAnswer(event) {
+  var selectedButton = event.target;
+  if (!selectedButton.classList.contains("choice")) {
+    return;
+  }
 
-//   var question = questions[currentQuestionIndex];
-//   var selectedAnswer = Array.from(choicesElement.children).indexOf(selectedButton);
+  var question = questions[currentQuestionIndex];
+  var selectedAnswer = Array.from(choicesElement.children).indexOf(selectedButton);
 
-//   if (selectedAnswer === question.correctAnswer) {
-//     score++;
-//   } else {
-//     timeLeft -= 10;
-//     if (timeLeft < 0) {
-//       timeLeft = 0;
-//     }
-//     timeLeftElement.textContent = timeLeft;
-//   }
+  if (selectedAnswer === question.correctAnswer) {
+    score++;
+  } else {
+    timeLeft -= 10;
+    if (timeLeft < 0) {
+      timeLeft = 0;
+    }
+    timeLeftElement.textContent = timeLeft;
+  }
 
-//   currentQuestionIndex++;
-//   if (currentQuestionIndex === questions.length) {
-//     endQuiz();
-//   } else {
-//     showQuestion();
-//   }
-// }
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    endQuiz();
+  } else {
+    showQuestion();
+  }
+}
 
-// // Ends the quiz
-// function endQuiz() {
-//   clearInterval(timerInterval);
-//   quizContainer.style.display = "none";
-//   gameOverElement.style.display = "block";
-//   scoreElement.textContent = score;
-//   playAgainButton.style.display = "block";
-// }
+// Ends the quiz
+function endQuiz() {
+  clearInterval(timerInterval);
+  quizContainer.style.display = "none";
+  gameOverElement.style.display = "block";
+  scoreElement.textContent = score;
+  playAgainButton.style.display = "block";
+}
 
-// // Resets the quiz
-// function resetQuiz() {
-//   gameOverElement.style.display = "none";
-//   playAgainButton.style.display = "none";
-//   startButton.style.display = "block";
-//   currentQuestionIndex = 0;
-//   timeLeft = 60;
-//   score = 0;
-// }
+// Resets the quiz
+function resetQuiz() {
+  gameOverElement.style.display = "none";
+  playAgainButton.style.display = "none";
+  startButton.style.display = "block";
+  currentQuestionIndex = 0;
+  timeLeft = 60;
+  score = 0;
+}
 
-// // Saves the score
-// function saveScore() {
-//   var initials = initialsInput.value.trim();
-//   if (initials !== "") {
-//     // Handle saving the initials and score, e.g., send them to a server or save in local storage
-//     alert("Score saved!");
-//   }
-// }
+// Saves the score
+function saveScore() {
+  var initials = initialsInput.value.trim();
+  if (initials !== "") {
+    // Handle saving the initials and score, e.g., send them to a server or save in local storage
+    alert("Score saved!");
+  }
+}
 
-// // Updates the timer
-// function updateTime() {
-//   timeLeftElement.textContent = timeLeft;
-//   if (timeLeft === 0) {
-//     endQuiz();
-//   } else {
-//     timeLeft--;
-//   }
-// }
+// Updates the timer
+function updateTime() {
+  timeLeftElement.textContent = timeLeft;
+  if (timeLeft === 0) {
+    endQuiz();
+  } else {
+    timeLeft--;
+  }
+}
